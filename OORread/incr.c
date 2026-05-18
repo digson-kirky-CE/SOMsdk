22e75ab5dad4680e2b87ccceeeb0ae685f6a77a6
poi114514-kemiao <3832687224@qq.com>
2026-05-18 17:40:56 +0800

    Add file copy feature to main function
    Implement functionality to copy a default file to a work directory if the '-d' or '-do' flag is provided.

---
+++ OORread/incr.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DEFAULT_FILE "data.txt"
+#define WORKDIR "./workdir"
+
+int main(int argc, char *argv[]) {
+    int do_copy = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-d") == 0 ||
+            strcmp(argv[i], "-do") == 0) {
+            do_copy = 1;
+        }
+    }
+
+    if (do_copy) {
+        system("mkdir -p workdir");
+        char cmd[512];
+        snprintf(cmd, sizeof(cmd),
+                 "cp %s %s/", DEFAULT_FILE, WORKDIR);
+        system(cmd);
+        printf("Copied to workdir\n");
+    }
+
+    // 启动 TUI
+    start_tui(DEFAULT_FILE);
+    return 0;
+}
