c37b68f32d1bab80dfd835326f7e825fc2d87f3b
poi114514-kemiao <3832687224@qq.com>
2026-05-18 17:43:04 +0800

    Update build script to handle new directory names

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 CC=clang
 CFLAGS=-Wall -O2
 LIBS=-lncurses
-cd $(ls | grep "OOR")
+cd $(ls | grep "OORw")
 $CC $CFLAGS source.c -o incw $LIBS
+cd ..
+cd  $(ls | grep "OORr")
+$CC $CFLAGS incr.c tui.c -o incr $LIBS
